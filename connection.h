cca3777454ebb90f9ae2d58b040bef82b693cdd9
cyrine <cyrine.maamer@esprit.tn>
2022-02-26 22:45:50 +0100

    Connection.h

---
+++ connection.h
@@ -0,0 +1,17 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
+
+class Connection
+{
+    QSqlDatabase db;
+public:
+    QSqlDatabase getDB(){return db;}
+    Connection();
+    bool createconnect();
+    void closeconnection();
+};
+
+#endif // CONNECTION_H

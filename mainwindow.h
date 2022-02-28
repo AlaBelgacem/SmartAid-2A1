1398b8a851d54cea3c8145d209046232cbf22ae8
emna <emna.darragi@esprit.tn>
2022-02-28 20:08:12 +0100

    first mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStackedWidget>
+#include <QAbstractItemView>
+#include <QString>
+#include <QDebug>
+#include <QListWidget>
+#include <QMessageBox>
+#include "dons.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_listWidget_itemClicked(QListWidgetItem *item);
+
+    void on_pushButton_ajouter_clicked();
+
+    void on_pushButton_supprimer_clicked();
+
+    void on_pushButton_modifier_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    Dons Etmp;
+};
+
+#endif // MAINWINDOW_H

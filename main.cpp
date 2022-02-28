0f900383b8f80f1c2bb0c3f853cdb47f3a67d36f
Essyl-Saidi <essyl.saidi@esprit.tn>
2022-02-28 19:49:36 +0100

    First commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    connection c;
+    MainWindow w;
+    bool test=c.createconnect();
+    if(test){
+        w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                   QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+
+    w.show();
+    return a.exec();
+}

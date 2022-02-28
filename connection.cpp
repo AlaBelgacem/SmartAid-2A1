2359667d375b5dd5eab24b904553024303767ac6
emna <emna.darragi@esprit.tn>
2022-02-28 19:59:44 +0100

    first connection.cpp

---
+++ connection.cpp
@@ -0,0 +1,22 @@
+#include "connection.h"
+
+connection::connection()
+{
+
+}
+
+
+bool connection::createconnect()
+{
+    bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("smart_aid");
+db.setUserName("Emna");//inserer nom de l'utilisateur
+db.setPassword("emna");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+    return  test;
+}

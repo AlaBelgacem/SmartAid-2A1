8ea60dbd9a0d9f4f7a4c2ef8b26bd07f55721071
Essyl Saidi <essyl.saidi@esprit.tn>
2022-04-04 21:38:52 +0100

    Final Version
    Final Version

---
+++ app - Final 2/mainwindow.h
@@ -0,0 +1,66 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "crises.h"
+#include "PieChart.h"
+#include <QMainWindow>
+#include <QStackedWidget>
+#include <QAbstractItemView>
+#include <QString>
+#include <QDebug>
+#include <QListWidget>
+#include <QMessageBox>
+#include "smtp.h"
+#include <QtWidgets/QMessageBox>
+#include <QFileDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_listWidget_itemClicked(QListWidgetItem *item);
+
+    void on_pushButton_ajouter_clicked();
+
+    void on_pushButton_supp_clicked();
+
+    void on_pushButton_modif_clicked();
+
+    void on_lineEditRecherche_textChanged(const QString &arg1);
+
+    void on_tableView_activated(const QModelIndex &index);
+
+    void on_radioButton_4_clicked();
+
+    void on_radioButton_5_clicked();
+
+    void on_pushButton_PDF_clicked();
+
+    void on_browseBtn_clicked();
+
+    void on_sendBtn_clicked();
+
+    void mailSent(QString);
+
+    void on_boutonAller_clicked();
+
+    void afficherPosition(QString latitude, QString longitude);
+
+
+
+
+
+private:
+    Ui::MainWindow *ui;
+    QStringList files;
+    Crises Cr; //object de type crises
+};
+#endif // MAINWINDOW_H
